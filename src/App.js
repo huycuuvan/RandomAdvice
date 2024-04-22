f16528d6d0350b034cea315014ae89fff956ac80
huycuuvan <huynd130904@gmail.com>
2024-04-22 21:16:10 +0700

    reactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { motion } from "framer-motion"
+import { useEffect, useState } from "react";
+import axios from 'axios'
 import './App.css';
+import React from "react";
 
 function App() {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+        handleClick()
+
+  }, [])
+
+     const handleClick  = async () => {
+      await axios.get('https://api.adviceslip.com/advice')
+        .then((result) => {
+          setQuote(result.data.slip);
+          
+          console.log(result.data.slip);
+        })
+        .catch((error) => {
+          
+          setQuote(error);
+        })
+      }
+      
+    
+    
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <h1>{`Advice#${quote.id}`}</h1>
+        <div className="text">
+          <p className="description">{quote.advice }</p>
+        </div>
+        <picture className="picture">
+          <img src="https://raw.githubusercontent.com/newton-w/advice-generator/22e8c826143baf25afa600f1968ec15bd87102f1/src/images/pattern-divider-desktop.svg"></img>
+        </picture>
+
+        <motion.div className="dice" whileTap={{rotate: 180}} onClick={handleClick} >
+          <img src="https://raw.githubusercontent.com/newton-w/advice-generator/22e8c826143baf25afa600f1968ec15bd87102f1/src/images/icon-dice.svg"></img>
+        
+        </motion.div>
+
+       
+      </div>
+       <div className="credit">
+          <span>Challenge by <a>frontEnd mentor.</a>Coded by <a href="https://www.facebook.com/nho.mattri.98031">Dinh Huy</a></span>
+        </div>
     </div>
   );
 }
